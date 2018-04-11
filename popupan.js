2a87f371464b95ef238e6e4a32295209f0e61f60
thanhlongbds3979 <33902526+thanhlongbds3979@users.noreply.github.com>
2018-04-11 20:29:38 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -2,7 +2,7 @@
 	var url_popup = 'https://banggiathanhnamdragon.blogspot.com';
 
       //<![CDATA[
-	  // var url_ppp = 'https://banggiathanhnamdragon.blogspot.com/';
+	  // var url_ppp = 'http://bit.ly/1KmPNYe';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
